cb98bfcc8f6b61fcab4181909838bb48b755faef
Pathum Disanayaka <kumadisanayaka18@gmail.com>
2026-09-11 19:41:42 +0530

    Implement SuperDefence class with area, strength, and message attributes and their respective getters and setters

--- src/model/SuperDefence.java
+++ src/model/SuperDefence.java
@@ -2,4 +2,38 @@
 package model;
 public class SuperDefence {
     
+    private String area;
+    private Strength strength;
+    private String message;
+
+    public SuperDefence(String area, Strength strength, String message) {
+        this.area = area;
+        this.strength = strength;
+        this.message = message;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public void setStrength(Strength strength) {
+        this.strength = strength;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public Strength getStrength() {
+        return strength;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+    
 }
