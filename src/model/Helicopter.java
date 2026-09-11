7311a259a8d539aa786d8d9b21358b3837c9f48b
Pathum Disanayaka <kumadisanayaka18@gmail.com>
2026-09-11 19:42:10 +0530

    Implement constructor and notifyObserver method in Helicopter class

--- src/model/Helicopter.java
+++ src/model/Helicopter.java
@@ -1,5 +1,17 @@
 
 package model;
 public class Helicopter extends SuperDefence implements Observable{
+
+    public Helicopter(String area, Strength strength, String message) {
+        super(area, strength, message);
+    }
+
+    @Override
+    public void notifyObserver(String area, Strength strength, String message) {
+        
+        
+    }
+    
+    
     
 }
