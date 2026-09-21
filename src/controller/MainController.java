8135b3103b2d556d80040c3922eff86a1917a628
Pathum Disanayaka <kumadisanayaka18@gmail.com>
2026-09-21 12:58:14 +0530

    Implement Observer interface in MainController and add updateInfo method

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,6 +1,13 @@
 
 package controller;
 
-public class MainController {
+import model.Observer;
+import model.Strength;
+
+public class MainController implements Observer{
+
+    @Override
+    public void updateInfo(String area, Strength strength, String message) {
+    }
     
 }
