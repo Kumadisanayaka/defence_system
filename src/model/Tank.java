255eb83168f52528df4cb8025101fbb3c898a779
Pathum Disanayaka <kumadisanayaka18@gmail.com>
2026-09-25 15:58:37 +0530

    Refactor Tank class to improve constructor parameters and enhance observer notification

--- src/model/Tank.java
+++ src/model/Tank.java
@@ -2,4 +2,24 @@
 package model;
 public class Tank extends SuperDefence implements Observable{
     
+    private int oxigen;
+
+    public Tank() {
+    }
+
+    public Tank(int oxigen) {
+        this.oxigen = oxigen;
+    }
+
+    public Tank(int oxigen, boolean area, String message, int soldierCount, int ammoCount, boolean position, Observer observer) {
+        super(area, message, soldierCount, ammoCount, position, observer);
+        this.oxigen = oxigen;
+    }
+    
+    @Override
+    public void notifyObserver(boolean area, String message, int soldierCount, int ammoCount, boolean position, Observer observer) {
+        observer.updateInfo(this, area, message, soldierCount, ammoCount, position);
+    }
+    
+    
 }
