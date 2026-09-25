fe919951105ab2cf14a0b5c22ed55c209262aeaf
Pathum Disanayaka <kumadisanayaka18@gmail.com>
2026-09-25 15:58:55 +0530

    Refactor SuperDefence class to update field types, enhance constructor, and improve getter/setter methods

--- src/model/SuperDefence.java
+++ src/model/SuperDefence.java
@@ -2,47 +2,108 @@
 package model;
 public class SuperDefence {
     
-    private String area;
-    private Strength strength;
+    private boolean area;
     private String message;
+    private int soldierCount;
+    private int ammoCount;
+    private boolean position; 
     private Observer observer;
 
-    public SuperDefence(String area, Strength strength, String message) {
+    public SuperDefence(boolean area, String message, int soldierCount, int ammoCount, boolean position,Observer observer) {
         this.area = area;
-        this.strength = strength;
         this.message = message;
-    }
-
-    public void setArea(String area) {
-        this.area = area;
-    }
-
-    public void setStrength(Strength strength) {
-        this.strength = strength;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public void setObserver(Observer observer) {
+        this.soldierCount = soldierCount;
+        this.ammoCount = ammoCount;
+        this.position = position;
         this.observer = observer;
     }
 
-    public String getArea() {
+    public SuperDefence() {
+        
+    } 
+
+    /**
+     * @return the area
+     */
+    public boolean isArea() {
         return area;
     }
 
-    public Strength getStrength() {
-        return strength;
+    /**
+     * @param area the area to set
+     */
+    public void setArea(boolean area) {
+        this.area = area;
     }
 
+    /**
+     * @return the message
+     */
     public String getMessage() {
         return message;
     }
 
+    /**
+     * @param message the message to set
+     */
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    /**
+     * @return the soldierCount
+     */
+    public int getSoldierCount() {
+        return soldierCount;
+    }
+
+    /**
+     * @param soldierCount the soldierCount to set
+     */
+    public void setSoldierCount(int soldierCount) {
+        this.soldierCount = soldierCount;
+    }
+
+    /**
+     * @return the ammoCount
+     */
+    public int getAmmoCount() {
+        return ammoCount;
+    }
+
+    /**
+     * @param ammoCount the ammoCount to set
+     */
+    public void setAmmoCount(int ammoCount) {
+        this.ammoCount = ammoCount;
+    }
+
+    /**
+     * @return the position
+     */
+    public boolean isPosition() {
+        return position;
+    }
+
+    /**
+     * @param position the position to set
+     */
+    public void setPosition(boolean position) {
+        this.position = position;
+    }
+
+    /**
+     * @return the observer
+     */
     public Observer getObserver() {
         return observer;
+    }
+
+    /**
+     * @param observer the observer to set
+     */
+    public void setObserver(Observer observer) {
+        this.observer = observer;
     }
     
 }
