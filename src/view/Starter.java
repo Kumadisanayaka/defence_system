7c418ddc07831720fed5deea16ea1ee73e6edc82
Pathum Disanayaka <kumadisanayaka18@gmail.com>
2026-09-25 15:57:41 +0530

    Refactor Starter class to initialize MainController and add dependency unit observers

--- src/view/Starter.java
+++ src/view/Starter.java
@@ -1,9 +1,19 @@
 package view;
 
+import controller.MainController;
+import model.Helicopter;
+import model.Tank;
+
 public class Starter {
 
     public static void main(String[] args) {
-        new HomePage().setVisible(true);
+        
+        MainController mainController = new MainController();
+        mainController.addDepenceUnitObserver(new Helicopter());
+        mainController.addDepenceUnitObserver(new Tank());
+        //mainController.addDepenceUnitObserver(new Submarine());
+        
+        new HomePage(mainController).setVisible(true);
         
     }
 }
