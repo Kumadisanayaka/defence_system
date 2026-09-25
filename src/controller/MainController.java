6860c8ae23be97964dffa9dcafc8f25402ac3d9f
Pathum Disanayaka <kumadisanayaka18@gmail.com>
2026-09-25 15:59:25 +0530

    Refactor MainController class to enhance observer management and updateInfo method signature for improved functionality

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,13 +1,42 @@
 
 package controller;
 
+import java.util.ArrayList;
 import model.Observer;
-import model.Strength;
+import model.SuperDefence;
 
 public class MainController implements Observer{
+    
+    private ArrayList<SuperDefence> depenceUnit = new ArrayList<>();
 
-    @Override
-    public void updateInfo(String area, Strength strength, String message) {
+    public void addDepenceUnitObserver(SuperDefence ob){
+        
+        if(ob != null){
+            depenceUnit.add(ob);
+        }
+    } 
+    
+    public void updateArea(boolean area){
+         
+        for (SuperDefence superDefence : depenceUnit) {
+            
+            superDefence.setArea(area);
+            
+        }
+        
     }
     
+    public void resiveMessege(String message){
+    
+        for (SuperDefence superDefence : depenceUnit) {
+            
+            superDefence.setMessage(message);
+            
+        }
+    }
+
+    @Override
+    public void updateInfo(SuperDefence unit, boolean area, String message, int soldierCount, int ammoCount, boolean position) {
+    
+    }
 }
