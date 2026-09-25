bfa595aac00064b776db36241a34d2351d4968f8
Pathum Disanayaka <kumadisanayaka18@gmail.com>
2026-09-25 15:59:06 +0530

    Refactor Submarine class to enhance constructor and implement observer notification method

--- src/model/Submarine.java
+++ src/model/Submarine.java
@@ -1,5 +1,19 @@
 
 package model;
 public class Submarine extends SuperDefence implements Observable{
+
+    public Submarine(boolean area, String message, int soldierCount, int ammoCount, boolean position, Observer observer) {
+        
+        super(area, message, soldierCount, ammoCount, position, observer);
+        
+    }
+    
+    
+
+    @Override
+    public void notifyObserver(boolean area, String message, int soldierCount, int ammoCount, boolean position, Observer observer) {
+        observer.updateInfo(this, area, message, soldierCount, ammoCount, position);
+    
+    }
     
 }
