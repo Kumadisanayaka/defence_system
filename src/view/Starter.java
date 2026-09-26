91c73303db2e6c80e01f04d9b8138523ef2a890e
Pathum Disanayaka <kumadisanayaka18@gmail.com>
2026-09-26 14:30:17 +0530

    Add Submarine observer to MainController in Starter class

--- src/view/Starter.java
+++ src/view/Starter.java
@@ -2,18 +2,19 @@ package view;
 
 import controller.MainController;
 import model.Helicopter;
+import model.Submarine;
 import model.Tank;
 
 public class Starter {
 
     public static void main(String[] args) {
-        
+
         MainController mainController = new MainController();
+
         mainController.addDepenceUnitObserver(new Helicopter());
         mainController.addDepenceUnitObserver(new Tank());
-        //mainController.addDepenceUnitObserver(new Submarine());
-        
+        mainController.addDepenceUnitObserver(new Submarine());
+
         new HomePage(mainController).setVisible(true);
-        
     }
 }
