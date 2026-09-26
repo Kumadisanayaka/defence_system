956a4dcabe93944c9f454e2c2186a7fa223bef53
Pathum Disanayaka <kumadisanayaka18@gmail.com>
2026-09-26 14:30:59 +0530

    Enhance Submarine class: add default constructor for improved instantiation

--- src/model/Submarine.java
+++ src/model/Submarine.java
@@ -2,14 +2,16 @@
 package model;
 public class Submarine extends SuperDefence implements Observable{
 
+    public Submarine() {
+        
+    }
+
     public Submarine(boolean area, String message, int soldierCount, int ammoCount, boolean position, Observer observer) {
         
         super(area, message, soldierCount, ammoCount, position, observer);
         
     }
     
-    
-
     @Override
     public void notifyObserver(boolean area, String message, int soldierCount, int ammoCount, boolean position, Observer observer) {
         observer.updateInfo(this, area, message, soldierCount, ammoCount, position);
